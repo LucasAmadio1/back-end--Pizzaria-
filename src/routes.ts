d1809ebaa6818c9a8862a8a84c6d98c275cf3489
LucasAmadio1 <lucasrochaamadio02@gmail.com>
2024-03-08 22:26:12 -0300

    iniciando o back projeto pizzaria

---
+++ src/routes.ts
@@ -0,0 +1,7 @@
+import { Router, Request, Response } from 'express'
+
+export const router = Router()
+  
+router.get('/test', (req: Request, res: Response) => {
+  throw new Error('Erro ao fazer a requisição')
+}) 
